84898714c31b655c1463c78fd442106b24af299e
Prashant Singh <sprash@codeaurora.org>
2015-07-02 16:43:30 -0700

    Data Call setup fixes.
    - Auto attach flag is true after the first data attach.
      When there is an on demand MMS request on non-DDS sub,
      MMS PDP setup is attempted even when data is out of service.
      This can be a performance hit as multiple SETUP_DATA_CALL
      requests can be sent down to RIL in OOS. Avoid this by
      considering auto attach flag only on DDS sub.
    
    - Use the subId from current phone to do retry as on demand
      PDP retries will fail otherwise.
    
    Conflicts:
    src/java/com/android/internal/telephony/dataconnection/DcTracker.java
    Change-Id: Iac7d5a226460ce08957ac10e222c8231a461a7d3
    CRs-Fixed: 865004

--- src/java/com/android/internal/telephony/dataconnection/DcTracker.java
+++ src/java/com/android/internal/telephony/dataconnection/DcTracker.java
@@ -779,7 +779,8 @@ public final class DcTracker extends DcTrackerBase {
             state = mPhone.getCallTracker().getState();
         }
         boolean allowed =
-                    (attachedState || mAutoAttachOnCreation.get()) &&
+                    (attachedState || (mAutoAttachOnCreation.get() &&
+                            (mPhone.getSubId() == dataSub))) &&
                     recordsLoaded &&
                     (state == PhoneConstants.State.IDLE ||
                     (subscriptionFromNv || recordsLoaded) &&
@@ -1507,12 +1508,12 @@ public final class DcTracker extends DcTrackerBase {
         intent.addFlags(Intent.FLAG_RECEIVER_FOREGROUND);
 
         // Get current sub id.
-        int subId = SubscriptionManager.getDefaultDataSubId();
+        int subId = mPhone.getSubId();
         intent.putExtra(PhoneConstants.SUBSCRIPTION_KEY, subId);
 
         if (DBG) {
             log("startAlarmForReconnect: delay=" + delay + " action=" + intent.getAction()
-                    + " apn=" + apnContext);
+                    + " apn=" + apnContext + " subId=" + subId);
         }
 
         PendingIntent alarmIntent = PendingIntent.getBroadcast (mPhone.getContext(), 0,
